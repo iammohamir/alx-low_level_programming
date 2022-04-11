37a01eb4e249b8319753649a0308a7945c4c17b4
root <root@DESKTOP-HL07JRR.localdomain>
2022-04-11 23:58:41 +0300

    ssssss

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,8 @@ for (j = i + 1; j <= '9'; j++)
 putchar(i);
 putchar(j);
 if (i != '8' || j != '9')
-																									{
-																									putchar(',');
-																									putchar(' ');
-
-
-																									}
+{																									putchar(',');
+   putchar(' ');																								}
 }
 }
 putchar('\n');
