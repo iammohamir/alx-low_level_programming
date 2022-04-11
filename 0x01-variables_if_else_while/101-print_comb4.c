02e573438bb4199179ee4dfaeab053abe2d0daa6
root <root@DESKTOP-HL07JRR.localdomain>
2022-04-12 00:08:52 +0300

    ww

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,13 +14,14 @@ for (j = i + 1; j <= '9'; j++)
 {
 for (k = j + 1; k <= '9'; k++)
 {
+
 putchar(i);
 putchar(j);
 putchar(k);											
 if (i != '7' || j != '8' || k != '9')
-{										
-putchar(',');			
-putchar(' ');					
+{
+putchar(',');
+putchar(' ');
 }
 }
 }
