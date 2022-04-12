34b0c72e490c9dfcaef0cb86d1d0bb10f6463a84
root <root@DESKTOP-HL07JRR.localdomain>
2022-04-12 18:53:35 +0300

    s

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -16,10 +16,12 @@ int main(void)
 		i = j;
 		j = k;
 		printf("%lu", k);
-if (count == 49)											putchar('\n');
-else
-{													printf(", ");
-}
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
 	}
 	return (0);
 }
